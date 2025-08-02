6554537ba487fafee6e5ec401b013b593a23a8d8
Tomas <tomas.aguilera.27@gmail.com>
2025-08-02 13:42:48 -0300

    Form localidades Sin acciones

---
+++ ResolucionAnomalias/WindowsForms/LocalidadApiLocalidad.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model;
+
+namespace WindowsForms
+{
+    public class LocalidadApiLocalidad
+    {
+        private static HttpClient client = new HttpClient();
+        static LocalidadApiLocalidad()
+        {
+            client.BaseAddress = new Uri("http://localhost:5183/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public static async Task<Localidad> GetAsync(int id)
+        {
+            Localidad localidad = null;
+            HttpResponseMessage response = await client.GetAsync("localidades/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                localidad = await response.Content.ReadAsAsync<Localidad>();
+            }
+            return localidad;
+        }
+
+        public static async Task<IEnumerable<Localidad>> GetAllAsync()      //En el ejemplo esta con dto el IEnumerable
+        {
+            IEnumerable<Localidad> localidades = null;
+            HttpResponseMessage response = await client.GetAsync("localidades");
+            if (response.IsSuccessStatusCode)
+            {
+                localidades = await response.Content.ReadAsAsync<IEnumerable<Localidad>>();
+            }
+            return localidades;
+        }
+
+        public async static Task AddAsync(Localidad localidad)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("localidades", localidad);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task DeleteAsync(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync("localidades/" + id);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task UpdateAsync(Localidad localidad)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync("localidades", localidad);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
+
---
+++ ResolucionAnomalias/WindowsForms/LocalidadDetalle.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsForms
+{
+    partial class LocalidadDetalle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelCodigoLoc = new Label();
+            labelNombreLoc = new Label();
+            buttonAceptarDetLoc = new Button();
+            buttonCancelarLocDet = new Button();
+            textBoxCodigoLoc = new TextBox();
+            textBoxNombreLoc = new TextBox();
+            SuspendLayout();
+            // 
+            // labelCodigoLoc
+            // 
+            labelCodigoLoc.AutoSize = true;
+            labelCodigoLoc.Location = new Point(95, 122);
+            labelCodigoLoc.Name = "labelCodigoLoc";
+            labelCodigoLoc.Size = new Size(65, 20);
+            labelCodigoLoc.TabIndex = 0;
+            labelCodigoLoc.Text = "Codigo: ";
+            labelCodigoLoc.Click += label1_Click;
+            // 
+            // labelNombreLoc
+            // 
+            labelNombreLoc.AutoSize = true;
+            labelNombreLoc.Location = new Point(95, 171);
+            labelNombreLoc.Name = "labelNombreLoc";
+            labelNombreLoc.Size = new Size(67, 20);
+            labelNombreLoc.TabIndex = 1;
+            labelNombreLoc.Text = "Nombre:";
+            labelNombreLoc.Click += label2_Click;
+            // 
+            // buttonAceptarDetLoc
+            // 
+            buttonAceptarDetLoc.Location = new Point(348, 244);
+            buttonAceptarDetLoc.Name = "buttonAceptarDetLoc";
+            buttonAceptarDetLoc.Size = new Size(94, 29);
+            buttonAceptarDetLoc.TabIndex = 2;
+            buttonAceptarDetLoc.Text = "Aceptar";
+            buttonAceptarDetLoc.UseVisualStyleBackColor = true;
+            buttonAceptarDetLoc.Click += button1_Click;
+            // 
+            // buttonCancelarLocDet
+            // 
+            buttonCancelarLocDet.Location = new Point(116, 244);
+            buttonCancelarLocDet.Name = "buttonCancelarLocDet";
+            buttonCancelarLocDet.Size = new Size(94, 29);
+            buttonCancelarLocDet.TabIndex = 3;
+            buttonCancelarLocDet.Text = "Cancelar";
+            buttonCancelarLocDet.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCodigoLoc
+            // 
+            textBoxCodigoLoc.BackColor = SystemColors.ControlLight;
+            textBoxCodigoLoc.Location = new Point(183, 122);
+            textBoxCodigoLoc.Name = "textBoxCodigoLoc";
+            textBoxCodigoLoc.ReadOnly = true;
+            textBoxCodigoLoc.Size = new Size(210, 27);
+            textBoxCodigoLoc.TabIndex = 4;
+            textBoxCodigoLoc.TextChanged += this.textBoxCodigoLoc_TextChanged;
+            // 
+            // textBoxNombreLoc
+            // 
+            textBoxNombreLoc.Location = new Point(183, 171);
+            textBoxNombreLoc.Name = "textBoxNombreLoc";
+            textBoxNombreLoc.Size = new Size(210, 27);
+            textBoxNombreLoc.TabIndex = 4;
+            textBoxNombreLoc.TextChanged += this.textBoxNombreLoc_TextChanged;
+            // 
+            // LocalidadDetalle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(613, 365);
+            Controls.Add(textBoxNombreLoc);
+            Controls.Add(textBoxCodigoLoc);
+            Controls.Add(buttonCancelarLocDet);
+            Controls.Add(buttonAceptarDetLoc);
+            Controls.Add(labelNombreLoc);
+            Controls.Add(labelCodigoLoc);
+            Name = "LocalidadDetalle";
+            Text = "LocalidadDetalle";
+            Load += LocalidadDetalle_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelCodigoLoc;
+        private Label labelNombreLoc;
+        private Button buttonAceptarDetLoc;
+        private Button buttonCancelarLocDet;
+        private TextBox textBoxCodigoLoc;
+        private TextBox textBoxNombreLoc;
+    }
+}
