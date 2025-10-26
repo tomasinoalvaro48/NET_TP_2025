82b770b525b372ce2e59ec884c8cf593a77c116d
Alvaro Tomasino <atomasino08@gmail.com>
2025-10-26 19:39:38 -0300

    cambios swagger

--- ResolucionAnomalias/WebAPI/Program.cs
+++ ResolucionAnomalias/WebAPI/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpLogging(o => { });
+// builder.Services.AddHttpLogging(o => { });
 
 // Add JWT Authentication
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
@@ -57,9 +57,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
-  app.UseHttpLogging();
+    // app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // app.UseHttpLogging();
 }
 
 app.UseHttpsRedirection();
