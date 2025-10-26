3210ffcb47ba5fb06070ccd5cf6caf22c1bb5a0c
Alvaro Tomasino <atomasino08@gmail.com>
2025-10-26 11:21:40 -0300

    Botones eliminados

--- ResolucionAnomalias/WindowsForms/MenuOperador.Designer.cs
+++ ResolucionAnomalias/WindowsForms/MenuOperador.Designer.cs
@@ -28,12 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            EnterCRUDLocalidadButton = new Button();
-            logoutButton = new Button();
-            EnterCRUDTipoAnomaliaButton = new Button();
-            btnCrudZona = new Button();
-            EnterCRUDUsuarioButton = new Button();
-            pedidoAgregacionButton = new Button();
             menuStripOperador = new MenuStrip();
             usuariosToolStripMenuItem = new ToolStripMenuItem();
             tiposDeAnomaliasToolStripMenuItem = new ToolStripMenuItem();
@@ -45,74 +39,6 @@
             menuStripOperador.SuspendLayout();
             SuspendLayout();
             // 
-            // EnterCRUDLocalidadButton
-            // 
-            EnterCRUDLocalidadButton.Location = new Point(306, 379);
-            EnterCRUDLocalidadButton.Margin = new Padding(4);
-            EnterCRUDLocalidadButton.Name = "EnterCRUDLocalidadButton";
-            EnterCRUDLocalidadButton.Size = new Size(279, 92);
-            EnterCRUDLocalidadButton.TabIndex = 1;
-            EnterCRUDLocalidadButton.Text = "CRUD Localidad";
-            EnterCRUDLocalidadButton.UseVisualStyleBackColor = true;
-            EnterCRUDLocalidadButton.Click += ButtonCRUDLocalidad_Click;
-            // 
-            // logoutButton
-            // 
-            logoutButton.AutoSize = true;
-            logoutButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            logoutButton.Location = new Point(709, 529);
-            logoutButton.Margin = new Padding(4);
-            logoutButton.Name = "logoutButton";
-            logoutButton.Size = new Size(162, 42);
-            logoutButton.TabIndex = 2;
-            logoutButton.Text = "Cerrar sesión";
-            logoutButton.UseVisualStyleBackColor = true;
-            logoutButton.Click += logoutButton_Click;
-            // 
-            // EnterCRUDTipoAnomaliaButton
-            // 
-            EnterCRUDTipoAnomaliaButton.Location = new Point(13, 377);
-            EnterCRUDTipoAnomaliaButton.Margin = new Padding(4);
-            EnterCRUDTipoAnomaliaButton.Name = "EnterCRUDTipoAnomaliaButton";
-            EnterCRUDTipoAnomaliaButton.Size = new Size(279, 92);
-            EnterCRUDTipoAnomaliaButton.TabIndex = 3;
-            EnterCRUDTipoAnomaliaButton.Text = "CRUD Tipo de Anomalia";
-            EnterCRUDTipoAnomaliaButton.UseVisualStyleBackColor = true;
-            EnterCRUDTipoAnomaliaButton.Click += ButtonCRUDTipoAnomalia_Click;
-            // 
-            // btnCrudZona
-            // 
-            btnCrudZona.Location = new Point(306, 479);
-            btnCrudZona.Margin = new Padding(6, 4, 6, 4);
-            btnCrudZona.Name = "btnCrudZona";
-            btnCrudZona.Size = new Size(279, 92);
-            btnCrudZona.TabIndex = 4;
-            btnCrudZona.Text = "CRUD Zona";
-            btnCrudZona.UseVisualStyleBackColor = true;
-            btnCrudZona.Click += btnCrudZona_Click;
-            // 
-            // EnterCRUDUsuarioButton
-            // 
-            EnterCRUDUsuarioButton.Location = new Point(13, 277);
-            EnterCRUDUsuarioButton.Margin = new Padding(4);
-            EnterCRUDUsuarioButton.Name = "EnterCRUDUsuarioButton";
-            EnterCRUDUsuarioButton.Size = new Size(279, 92);
-            EnterCRUDUsuarioButton.TabIndex = 5;
-            EnterCRUDUsuarioButton.Text = "CRUD Usuario";
-            EnterCRUDUsuarioButton.UseVisualStyleBackColor = true;
-            EnterCRUDUsuarioButton.Click += ButtonCRUDUsuario_Click;
-            // 
-            // pedidoAgregacionButton
-            // 
-            pedidoAgregacionButton.Location = new Point(15, 479);
-            pedidoAgregacionButton.Margin = new Padding(6);
-            pedidoAgregacionButton.Name = "pedidoAgregacionButton";
-            pedidoAgregacionButton.Size = new Size(279, 92);
-            pedidoAgregacionButton.TabIndex = 6;
-            pedidoAgregacionButton.Text = "CRUD Pedido de Agregacion";
-            pedidoAgregacionButton.UseVisualStyleBackColor = true;
-            pedidoAgregacionButton.Click += pedidoAgregacionButton_Click;
-            // 
             // menuStripOperador
             // 
             menuStripOperador.BackColor = SystemColors.Control;
@@ -120,7 +46,7 @@
             menuStripOperador.Items.AddRange(new ToolStripItem[] { usuariosToolStripMenuItem, tiposDeAnomaliasToolStripMenuItem, localidadesToolStripMenuItem, zonasToolStripMenuItem, pedidosDeAgregacionToolStripMenuItem, toolStripMenuItemMasAjustes });
             menuStripOperador.Location = new Point(0, 0);
             menuStripOperador.Name = "menuStripOperador";
-            menuStripOperador.Size = new Size(1213, 40);
+            menuStripOperador.Size = new Size(1213, 42);
             menuStripOperador.TabIndex = 7;
             menuStripOperador.Text = "menuStripOperador";
             // 
@@ -179,12 +105,6 @@
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1213, 606);
-            Controls.Add(pedidoAgregacionButton);
-            Controls.Add(EnterCRUDUsuarioButton);
-            Controls.Add(btnCrudZona);
-            Controls.Add(EnterCRUDTipoAnomaliaButton);
-            Controls.Add(logoutButton);
-            Controls.Add(EnterCRUDLocalidadButton);
             Controls.Add(menuStripOperador);
             MainMenuStrip = menuStripOperador;
             Margin = new Padding(4);
@@ -197,12 +117,6 @@
         }
 
         #endregion
-        private Button EnterCRUDLocalidadButton;
-        private Button logoutButton;
-        private Button EnterCRUDTipoAnomaliaButton;
-        private Button btnCrudZona;
-        private Button EnterCRUDUsuarioButton;
-        private Button pedidoAgregacionButton;
         private MenuStrip menuStrip1;
         private ToolStripMenuItem usuariosToolStripMenuItem;
         private ToolStripMenuItem tiposDeAnomaliasToolStripMenuItem;
