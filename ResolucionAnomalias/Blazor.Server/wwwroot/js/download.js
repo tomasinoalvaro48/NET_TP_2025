259b877ec4d166a76f83d6349f7fe28a173706e8
Tomas <tomas.aguilera.27@gmail.com>
2025-10-27 22:32:27 -0300

    Reportes en blazor sin graficos

---
+++ ResolucionAnomalias/Blazor.Server/wwwroot/js/download.js
@@ -0,0 +1,15 @@
+window.downloadFileFromBytes = (fileName, contentType, base64Data) => {
+  const byteCharacters = atob(base64Data);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) byteNumbers[i] = byteCharacters.charCodeAt(i);
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], { type: contentType });
+
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  URL.revokeObjectURL(link.href);
+  link.remove();
+};
